7de3744dff133db04d05232cfb56bc1208a0e4b0
Marcos Alves <limafilhoguerreiro@outlook.com>
2022-11-24 21:21:59 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,12 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
+    const requiredFields = ['name', 'email']
 
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    for (const field in requiredFields) {
+      if (!httpRequest.body[requiredFields[field]]) {
+        return badRequest(new MissingParamError(requiredFields[field]))
+      }
     }
 
     return {
