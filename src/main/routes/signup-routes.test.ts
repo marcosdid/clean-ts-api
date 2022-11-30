637238d0961dd6146a44e8be2349f0df6e94232d
marcosdid <limafilhoguerreiro@outlook.com>
2022-11-30 20:58:36 -0300

    refactor: connecr to mongodb before intergration test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('SignUo Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconect()
+  })
+
+  beforeEach(async () => {
+    const accountColletion = await MongoHelper.getColletion('accounts')
+    await accountColletion.deleteMany({})
+  })
+
   test('Should return an account on sucess', async () => {
     await request(app)
       .post('/api/signup')
