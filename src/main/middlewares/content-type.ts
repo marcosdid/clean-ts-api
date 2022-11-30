f15e3f2c94823c0ae70ead4f37754d16e49cc717
marcosdid <limafilhoguerreiro@outlook.com>
2022-11-30 11:56:12 -0300

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { Request, Response, NextFunction } from 'express'
+
+export const contentType = (req: Request, res: Response, next: NextFunction): void => {
+  res.type('json')
+  next()
+}
--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { Request, Response, NextFunction } from 'express'
 
-export const cors = (req: Request, res: Response, next: NextFunction) => {
+export const cors = (req: Request, res: Response, next: NextFunction): void => {
   res.set('acess-control-allow-origin', '*')
   res.set('acess-control-allow-methods', '*')
   res.set('acess-control-allow-headers', '*')
