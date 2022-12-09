d7e73f4c210cd876f7d77cc84fcd3cbcde01660a
marcosdid <limafilhoguerreiro@outlook.com>
2022-12-09 01:11:39 -0300

    feat: signup-routes

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,8 @@
 export default {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
